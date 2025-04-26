a41d94ed3bb65229423f2433e88d905bfa57677c
javeriatanveerdeveloper <alihussainuf456@gmail.com>
2025-04-26 11:28:12 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('WELCOME! OUR  VALUED CUSTOMERS');
+let item=prompt('Name the item you want to purshase :');
+let price=prompt('specify the price of the item :');
+let quantity=prompt('Write the quantity of the item you purshased ');
+console.log(`you purshased ${item} of price  ${price} in quantity ${quantity} and your subtotal is  ${price * quantity}` );
